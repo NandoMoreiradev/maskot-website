7ecf76b573dd10a3abcc5e5271e6002ca320c968
Fernando Moreira <contato@nandomoreira.com.br>
2026-02-03 06:36:54 -0300

    fix: build config for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    compiler: {
-        styledComponents: true,
-    },
-    output: 'export',
-    trailingSlash: true,
-    images: {
-        unoptimized: true,
-    },
+  compiler: {
+    styledComponents: true, // Garante CSS sem flicker
+  },
+  // output: 'export', // <--- REMOVIDO: Isso libera o servidor Node.js da Vercel
+  images: {
+    // unoptimized: true, // <--- REMOVIDO: Agora a Vercel vai otimizar suas imagens automaticamente
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.prismic.io', // Permite otimizar imagens do seu CMS
+      },
+    ],
+  },
 };
 
 export default nextConfig;
